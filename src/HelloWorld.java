662bd690fccb1003a781800ef8a2fdd80da52893
mosesli <mosesli@yeah.net>
2014-07-02 22:32:54 +0800

    Change HelliWorld.class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,12 +2,16 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world!");
+		print();
 		int x;
 		int y;
 		x=0;y=8;
 		int a = x+y;
 		System.out.println("a= "+ a);
 		return;
+	}
+	public static void print(){
+		System.out.println("Just change");
 	}
 
 }
